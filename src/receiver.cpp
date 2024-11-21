4f7a104ba69c646c9598cf0c91f284956b128b93
Luca-G49 <ghostnet002@gmail.com>
2024-11-21 20:58:25 +0000

    Removed unused functions

--- src/receiver.cpp
+++
@@ -1,8 +0,0 @@
-#include "receiver.h"
-#include <iostream>
-
-// Start listening for incoming AFSK tones and decode them
-void start_receiver() {
-    // TODO: Implement real-time decoding of AFSK tones using techniques like FFT
-    std::cout << "Receiver is now listening..." << std::endl;
-}

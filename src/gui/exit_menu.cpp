17322814c57ae1db763c31324228bd3173d7f2a3
Luca-G49 <ghostnet002@gmail.com>
2024-11-26 20:27:10 +0000

    Update exit message

--- src/gui/exit_menu.cpp
+++ src/gui/exit_menu.cpp
@@ -4,10 +4,9 @@
 #include "gui_utils.h"
 #include <thread>
 
-
 // Exit menu
 void exit_menu(){
     clearTerminal();
-    printBorder("Goodbye, thank you for using the application!");
+    printBorder("Goodbye, thank you for using PiAFSKLink!");
     std::this_thread::sleep_for(std::chrono::seconds(2));
 }
